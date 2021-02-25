64fcfbf3d1f87d91e83b5948bd205642b1e49a2d
EliMacMun <tecno_eli@hotmail.com>
2021-02-24 22:07:18 -0600

    correcion practica 3

--- Practicas/Practica 3/practica3.cpp
+++ Practicas/Practica 3/practica3.cpp
@@ -4,12 +4,15 @@ using namespace std;
 
 int main() {
     double horas, paga, total;
+    int unidades;
     cout << "Ingrese la cantidad de paga por hora" << endl;
     cin >> paga;
     cout << "\nIngrese las horas trabajadas" << endl;
     cin >> horas;
+    cout << "\nIngrese las unidades producidas" << endl;
+    cin >> unidades;
     total = horas*paga;
-    if (horas > 100) {
+    if (unidades > 100) {
         total += total*0.4;
     }
     cout << "\nTotal: " << total << endl;
